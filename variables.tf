9aebe5fd4d1ff213fd62ecd546239f36bd0a1036
chan <bcchoi@deleo.co.kr>
2025-10-22 12:02:04 +0900

    fix: cluster issuer missing spec added

--- variables.tf
+++ variables.tf
@@ -71,7 +71,7 @@ variable "aws_load_balancer_controller" {
 variable "external_dns_access_key_id" {
   description = "external-dns aws route53 access key id"
   type        = any
-  default     = {}
+  default     = ""
 }
 variable "cert_manager_zones" {
   description = "cert-manager zone list"
